95799b1623e30417f8143a6a62c8e42a8b28edc8
BradleyRyu <rrsky0322@kau.kr>
2021-06-01 16:11:22 +0900

    Moving my file

---
+++ EmbeddedSWProj.py
@@ -0,0 +1,8 @@
+import time
+import random
+from colorsys import hsv_to_rgb
+import board
+from digitalio import DigitalInOut, Direction
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
